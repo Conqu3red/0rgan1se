5a3b84568d84a0fc56a3cb36186579768c06aea3
JbCoder <JbCoder@users.noreply.github.com>
2019-12-08 20:27:31 +0000

    Update

--- organise.py
+++ organise.py
@@ -2,29 +2,24 @@ import os, sys
 from datetime import datetime
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
-from progress.bar import *
 from tkinter import *
 from tkinter.ttk import *
 from functools import partial
 tk=Tk()
 progress=Progressbar(tk,orient=HORIZONTAL,length=100,mode='determinate')
-#mod_time = os.path.getmtime("test.txt")
-#print(datetime.utcfromtimestamp(mod_time).strftime('%Y-%m-%d %H:%M:%S'))
 def organise(dire, file_end):
 	files = []
 	import os
-	#file_end = ".jpg"
-	#dire = askdirectory(title='Select the photos where your files are stored')
 	os.chdir(dire)
 	directory = os.fsencode(dire)
 	
+	# Get files from FOLDER
 	for file in os.listdir():
 		filename = os.fsdecode(file)
 		if filename.endswith(file_end):
 			files.append(str(filename))
-	#bar = IncrementalBar('Renaming', max=len(files))
-	#print("Files: [0/"+str(len(files)))
 
+	# Loop to do bulk renaming
 	for file in enumerate(files):
 		mod_time = os.path.getmtime(file[1])
 		modd = datetime.utcfromtimestamp(mod_time).strftime('%Y-%m-%d___%H-%M-%S')
@@ -32,18 +27,13 @@ def organise(dire, file_end):
 		new_file = os.path.join(modd + file_end)
 	
 		os.rename(old_file, new_file)
-		#print(str(old_file) + " --> " + str(new_file))
-		#os.system("cls")
-		#print("Files: [" + str(file[0]) + "/" + str(len(files))+"]")
-		#bar.next()
 		progress["value"] = int((file[0]/len(files))*100)
 		progress.pack()
 	t.insert(END, "\nDone!")  
 	btn.pack_forget()
-	#sys.exit()
-	#bar.finish()
+
+# TKINTER window
 dire = askdirectory(title='Select the folder where your files are stored')
-#organise(dire, ".jpg")
 progress.pack()
 command = partial(organise, dire, ".jpg")
 btn = Button(tk,text='Start',command=command)
