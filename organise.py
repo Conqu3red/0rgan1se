2e2c8bae41840c6705f08ed4f0bf113e9510de39
JbCoder <JbCoder@users.noreply.github.com>
2019-12-08 20:23:06 +0000

    Update organise.py

--- organise.py
+++ organise.py
@@ -3,6 +3,11 @@ from datetime import datetime
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
 from progress.bar import *
+from tkinter import *
+from tkinter.ttk import *
+from functools import partial
+tk=Tk()
+progress=Progressbar(tk,orient=HORIZONTAL,length=100,mode='determinate')
 #mod_time = os.path.getmtime("test.txt")
 #print(datetime.utcfromtimestamp(mod_time).strftime('%Y-%m-%d %H:%M:%S'))
 def organise(dire, file_end):
@@ -17,7 +22,7 @@ def organise(dire, file_end):
 		filename = os.fsdecode(file)
 		if filename.endswith(file_end):
 			files.append(str(filename))
-	bar = IncrementalBar('Renaming', max=len(files))
+	#bar = IncrementalBar('Renaming', max=len(files))
 	#print("Files: [0/"+str(len(files)))
 
 	for file in enumerate(files):
@@ -30,7 +35,20 @@ def organise(dire, file_end):
 		#print(str(old_file) + " --> " + str(new_file))
 		#os.system("cls")
 		#print("Files: [" + str(file[0]) + "/" + str(len(files))+"]")
-		bar.next()
-	bar.finish()
+		#bar.next()
+		progress["value"] = int((file[0]/len(files))*100)
+		progress.pack()
+	t.insert(END, "\nDone!")  
+	btn.pack_forget()
+	#sys.exit()
+	#bar.finish()
 dire = askdirectory(title='Select the folder where your files are stored')
-organise(dire, ".jpg")
+#organise(dire, ".jpg")
+progress.pack()
+command = partial(organise, dire, ".jpg")
+btn = Button(tk,text='Start',command=command)
+btn.pack()
+t = Text()
+t.insert(INSERT, "Selected Directory: "+dire)
+t.pack()
+mainloop()
