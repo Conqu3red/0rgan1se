68b3fee2b2650b79197b6fa1a33d05bb8ba9dcf0
JbCoder <JbCoder@users.noreply.github.com>
2019-12-23 08:44:26 +0000

    You can now format the name layout

--- organise.py
+++ organise.py
@@ -1,5 +1,5 @@
 import os, sys
-from datetime import datetime
+from datetime import datetime, timedelta
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
 from tkinter import *
@@ -8,8 +8,11 @@ from functools import partial
 tk=Tk()
 tk.resizable(False, False)
 progress=Progressbar(tk,orient=HORIZONTAL,length=100,mode='determinate')
-def organise(dire, file_end):
+def organise(dire, file_end, time_format='%Y-%m-%d___%H-%M-%S'):
 	files = []
+	new_file = ""
+	c = 0
+	file_names = {}
 	import os
 	os.chdir(dire)
 	directory = os.fsencode(dire)
@@ -20,14 +23,30 @@ def organise(dire, file_end):
 		if filename.endswith(file_end):
 			files.append(str(filename))
 
+	#for file in files:
+	#	file_names[file.split(file_end)[0]] = 0
 	# Loop to do bulk renaming
 	for file in enumerate(files):
 		mod_time = os.path.getmtime(file[1])
-		modd = datetime.utcfromtimestamp(mod_time).strftime('%Y-%m-%d___%H-%M-%S')
+		modd = (datetime.utcfromtimestamp(mod_time)+timedelta(hours=1)).strftime(time_format)
 		old_file = file[1]
-		new_file = os.path.join(modd + file_end)
-	
-		os.rename(old_file, new_file)
+		
+		new_file = os.path.join(modd)
+		f_new_file = new_file
+
+		# Add file base to dictionary if not already done so
+		if file_names.get(new_file) == None:
+			file_names[new_file] = 0
+		
+		# handle duplicate file names by appending _<number>
+		while os.path.exists(os.path.join(f_new_file + file_end)):
+			file_names[new_file] += 1
+			f_new_file = os.path.join(new_file + "_"+str(file_names[new_file]))
+		os.rename(old_file, os.path.join(f_new_file+file_end))
+		
+		#print(file_names)
+
+		# increment progress bar
 		progress["value"] = int((file[0]+1/len(files))*100)
 		progress.grid(row=0,column=1,columnspan=1)
 	#t.insert(END, "\nDone!")  
@@ -45,7 +64,7 @@ btn = Button(text='Start')
 def askdir():
 	dire = askdirectory(title='Select the folder where your files are stored')
 	print(dire)
-	btn = Button(text='Start',command=lambda: organise(dire, ".jpg"))
+	btn = Button(text='Start',command=lambda: organise(dire, ".jpg", input_box.get("1.0",'end-1c')))
 	btn.grid(row=0,column=0)
 	t.insert(END, dire)
 	#start = partial(organise, dire, ".jpg")
@@ -59,6 +78,19 @@ askdirebtn.grid(row=1,column=0)
 
 t = Text(width=50, height=2)
 t.insert(INSERT, "Selected Directory: ")
-t.grid(row=1,column=1, columnspan=1)
+t.grid(row=1,column=1, columnspan=1, rowspan=2)
 
+# .config(state=DISABLED)
+# Format name row
+
+
+format_t = Text(width=15,height=1)
+format_t.configure(state='normal')
+format_t.insert(INSERT, "Name Format:")
+format_t.configure(state='disabled')
+format_t.grid(row=4,column=0)
+
+input_box = Text(width=30,height=1)
+input_box.insert(INSERT, '%Y-%m-%d___%H-%M-%S')
+input_box.grid(row=4,column=1)
 mainloop()
